ef70d4a00c597c4b6e8c7274ed12556c4f7779f4
mansonase <mansonase@gmail.com>
2020-09-30 10:32:44 +0800

    10/31 implement shadow of unit and refactor constrainslayout into relativelayout

--- app/src/main/java/com/example/stationgx/pages/mainbaseactivity/MainBaseActivity.kt
+++ app/src/main/java/com/example/stationgx/pages/mainbaseactivity/MainBaseActivity.kt
@@ -3,14 +3,23 @@ package com.example.stationgx.pages.mainbaseactivity
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.stationgx.R
 import com.example.stationgx.pages.BaseActivity
+import com.example.stationgx.pages.home.HomeFragment
+import com.example.stationgx.pages.welcome.WelcomeFragment
+import kotlinx.android.synthetic.main.main_base.*
 import javax.inject.Inject
 
 class MainBaseActivity: BaseActivity() {
 
     @Inject
     lateinit var shared:SharedPreferences
+
+    //private lateinit var viewPager:ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +29,39 @@ class MainBaseActivity: BaseActivity() {
 
         if (isSaved)
             Log.d("main base","got return true")
+
+        val pagerAdapter=MainPagerAdapter(this)
+        base_viewpager.adapter=pagerAdapter
+
+        pageindicators.count=2
+
+        base_viewpager?.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                pageindicators.selection=position
+            }
+        })
     }
 
+
+
+    private inner class MainPagerAdapter(mainBaseActivity: MainBaseActivity) :FragmentStateAdapter(mainBaseActivity){
+        override fun getItemCount(): Int {
+            return 2
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            when(position){
+                0->{
+                    pageindicators.selection=0
+                    return WelcomeFragment()
+                }
+                1->{
+                    pageindicators.selection=1
+                    return HomeFragment()
+                }
+            }
+            return HomeFragment()
+        }
+    }
 
 }
