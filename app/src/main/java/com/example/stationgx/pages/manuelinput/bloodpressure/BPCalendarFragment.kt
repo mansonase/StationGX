ba807acba6c3952ebd03f2322c09712bd7bc5478
mansonase <mansonase@gmail.com>
2020-11-20 18:20:30 +0800

    11/20 calendar, bug: no scheme found even if i try to set

--- app/src/main/java/com/example/stationgx/pages/manuelinput/bloodpressure/BPCalendarFragment.kt
+++ app/src/main/java/com/example/stationgx/pages/manuelinput/bloodpressure/BPCalendarFragment.kt
@@ -52,6 +52,12 @@ class BPCalendarFragment : DialogFragment(){
         transaction.add(R.id.calendar_switcher, switcher!!,TAG_CALENDAR_SWITCHER)
         transaction.commit()
 
+        val calendar=Calendar()
+        calendar.year=2020
+        calendar.month=10
+        calendar.day=20
+        setBackgroundScheme(calendar,"today")
+
     }
 
 
@@ -101,12 +107,17 @@ class BPCalendarFragment : DialogFragment(){
         Log.d("bpcalendar","test function")
     }
 
-    fun setBackgroundScheme(calendar: Calendar,duration:String){
+    private fun setBackgroundScheme(calendar: Calendar, duration:String){
 
         var days=0
         when(duration){
-            "today"->days=1
-            "week"->days=7
+            "today"->{
+                days=1
+                setTodayScheme(calendar)
+            }
+            "week"->{
+                days=7
+            }
             "month"->{
                 when(calendar.month){
                     1,3,5,7,8,10,12->{
@@ -122,39 +133,122 @@ class BPCalendarFragment : DialogFragment(){
                 }
             }
         }
-        val year=calendar.year
-        val month=calendar.month
-        var day=calendar.day
+
+
+
+/*
+
         val map= hashMapOf<String,Calendar>()
 
+        var year=calendar.year
+        var month=calendar.month
+        var day: Int
 
-        map["mark"]=getSingleScheme(year,month,day,0x0288D1,"today")
 
-        if (days>1){
-            for (i in 0..days) {
-                map["mark"] = getSingleScheme(year, month, (day + i), 0xF9EBEC, "range")
+        // 用原生API找出現在的日期
+        val calendarReal=java.util.Calendar.getInstance()
+
+
+        //如果days==1,表示就是系統日期的今天
+        if (days==1){
+            map["mark"]=getSingleScheme(
+                                calendarReal.get(java.util.Calendar.YEAR)+0,
+                              calendarReal.get(java.util.Calendar.MONTH)+1,
+                                 calendarReal.get(java.util.Calendar.DAY_OF_MONTH)+0,
+                                        "today")
+
+        }
+        //days!=0 表示有範圍, 不止1天而已
+        else{
+
+            day=calendar.day
+
+            if (days>1){
+
+                // if week or month
+                for (i in 0..days) {
+
+                    if (day+i>days){// week 有over這個月
+
+                        if (month+1>12){//有over今年
+
+                            month=1
+                            year+=1
+                            day=1
+                        }else{//沒有over今年
+
+                            month+=1
+                            day=1
+                        }
+
+                        map["mark"] = getSingleScheme(year, month, (day + i-days),  "range")
+
+
+                    }else {// week沒有過這個月
+                        map["mark"] = getSingleScheme(year, month, (day + i),  "range")
+                    }
+                }
             }
+
+            if (calendar.year == calendarReal.get(java.util.Calendar.YEAR)&&calendar.month==(calendarReal.get(java.util.Calendar.MONTH)+1)){
+                // 判斷現在展示的這個月曆 是今年&&這個月, 如果是的話 要加 today的mark
+                //                                      如果是別的月份 就不用加today's mark
+
+                day=calendarReal.get(java.util.Calendar.DAY_OF_MONTH)
+                map["mark"]=getSingleScheme(year,month,day,"both")
+            }
+
+
         }
 
-        map["mark"]=getSingleScheme(year,month,4,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,5,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,6,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,7,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,8,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,9,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,10,0x229AD7,"record")
-        map["mark"]=getSingleScheme(year,month,11,0x229AD7,"record")
+        // for example mark some date to show record
+        map["mark"]=getSingleScheme(year,month,4,"record")
+        map["mark"]=getSingleScheme(year,month,5,"record")
+        map["mark"]=getSingleScheme(year,month,6,"record")
+        map["mark"]=getSingleScheme(year,month,7,"record")
+        map["mark"]=getSingleScheme(year,month,8,"record")
+        map["mark"]=getSingleScheme(year,month,9,"record")
+        map["mark"]=getSingleScheme(year,month,10,"record")
+        map["mark"]=getSingleScheme(year,month,11,"record")
 
         calendarView.setSchemeDate(map)
+
+ */
     }
 
-    private fun getSingleScheme(year:Int,month:Int,day:Int,color:Int,text:String):Calendar{
+    private fun getSingleScheme(year:Int,month:Int,day:Int,text:String):Calendar{
         val calendar=Calendar()
         calendar.year=year
         calendar.month=month
         calendar.day=day
-        calendar.schemeColor=color
-        calendar.scheme=text
+
+        if (text=="today"||text=="range"||text=="both"){
+            calendar.scheme=text
+        }else{
+            Log.d("bpcalendarff", "$text,$year year,  $month month, $day day")
+            //calendar.addScheme(0x229AD7,text)
+            calendar.scheme=text
+        }
+        //calendar.scheme=text
         return calendar
+    }
+
+
+    private fun setTodayScheme(calendar: Calendar){
+
+        val map= hashMapOf<String,Calendar>()
+        val calendar=getSingleScheme(calendar.year,calendar.month,calendar.day,"record")
+
+        map.put("mark",calendar)
+
+
+        calendarView.setSchemeDate(map)
+        calendarView.invalidate()
+
+    }
+    private fun setWeekScheme(calendar: Calendar, days:Int){
+
+    }
+    private fun setMonthScheme(calendar: Calendar, days: Int){
     }
 }
--- app/src/main/java/com/example/stationgx/ui/custom/calendar/CalendarMonth.kt
+++ app/src/main/java/com/example/stationgx/ui/custom/calendar/CalendarMonth.kt
@@ -2,8 +2,9 @@ package com.example.stationgx.ui.custom.calendar
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
+import android.util.Log
+import androidx.core.content.res.ResourcesCompat
 import com.example.stationgx.R
 import com.haibin.calendarview.Calendar
 import com.haibin.calendarview.MonthView
@@ -20,26 +21,42 @@ class CalendarMonth(context: Context) :MonthView(context) {
     private val paintEventDates=Paint(Paint.ANTI_ALIAS_FLAG)
     private val paintRangeDates=Paint(Paint.ANTI_ALIAS_FLAG)
 
+    private val calendarNow=java.util.Calendar.getInstance()
+    private val calendarCompare=java.util.Calendar.getInstance()
+
     init {
+        val font=ResourcesCompat.getFont(context,R.font.roboto_bold)
 
-        lastMonthDates.color= context.getColor(android.R.color.transparent)
+        lastMonthDates.color= resources.getColor(android.R.color.transparent,null)
 
-        nextMonthDates.color=context.getColor(R.color.frame_grey)
+        nextMonthDates.color=resources.getColor(R.color.frame_grey,null)
         nextMonthDates.textSize=24f
+        nextMonthDates.typeface=font
 
-        currentMonthPassedDates.color=context.getColor(R.color.word_black)
+        currentMonthPassedDates.color=resources.getColor(R.color.word_black,null)
         currentMonthPassedDates.textSize=24f
+        currentMonthPassedDates.typeface = font
 
-        currentMonthComingDates.color=context.getColor(R.color.icon_grey)
+
+        currentMonthComingDates.color=resources.getColor(R.color.icon_grey,null)
         currentMonthComingDates.textSize=24f
+        currentMonthComingDates.typeface=font
 
-        paintToday.color=context.getColor(R.color.main_white)
-        paintTodayCircle.color=context.getColor(R.color.word_blue)
+        paintToday.color=resources.getColor(R.color.main_white,null)
+        paintToday.textSize=24f
+        paintToday.typeface=font
+
+        paintTodayCircle.color=resources.getColor(R.color.word_blue,null)
         paintTodayCircle.style=Paint.Style.FILL
-        paintEventDates.color=context.getColor(R.color.border_blue)
+
+        paintEventDates.color=resources.getColor(R.color.border_blue,null)
         paintEventDates.style=Paint.Style.STROKE
         paintEventDates.strokeWidth=2f
-        paintRangeDates.color=context.getColor(R.color.background_pink)
+
+
+        Log.d("init","testingggg")
+        paintRangeDates.color=resources.getColor(R.color.background_pink,null)
+        paintRangeDates.style=Paint.Style.FILL
     }
 
     override fun onDrawSelected(canvas: Canvas?, calendar: Calendar?, x: Int, y: Int, hasScheme: Boolean): Boolean {
@@ -51,36 +68,127 @@ class CalendarMonth(context: Context) :MonthView(context) {
         val centerX=x+mItemWidth/2
         val centerY=y+mItemHeight/2
 
+        if (calendar!=null) {
+            Log.d("ondrawscheme", "${calendar.day}")
+        }else{
+            Log.d("ondrawscheme", "null , $x")
+        }
 
-        val schemes=calendar?.schemes
-
-        if (schemes?.get(0)?.scheme == "single"){
+        if (calendar?.scheme=="today"){
+            // draw today's circle and border
             canvas?.drawCircle(centerX.toFloat(),centerY.toFloat(),(mItemWidth/2).toFloat(),paintTodayCircle)
             canvas?.drawCircle(centerX.toFloat(),centerY.toFloat(),(mItemWidth/2).toFloat(),paintEventDates)
-        }else if (schemes?.get(0)?.scheme=="range"){
+        }else if (calendar?.scheme=="range"||calendar?.scheme=="both"){
 
+            // draw this month or week's background
             val dayOfWeek=CalendarUtil.getWeekFromCalendar(calendar)
             if (dayOfWeek==1.or(0)){
+                // if this day is monday(the most left position) or sunday(the most right position), draw circle
                 canvas?.drawCircle(centerX.toFloat(),centerY.toFloat(),(mItemWidth/2).toFloat(),paintRangeDates)
             }else{
+                // else these background is drew in rectangle
                 canvas?.drawRect(x.toFloat(),y.toFloat(),x+mItemHeight.toFloat(),(y+mItemHeight).toFloat(),paintRangeDates)
+            }
+
+            if (calendar.scheme =="both"){
+
+                canvas?.drawCircle(centerX.toFloat(),centerY.toFloat(),(mItemWidth/2).toFloat(),paintTodayCircle)
+                canvas?.drawCircle(centerX.toFloat(),centerY.toFloat(),(mItemWidth/2).toFloat(),paintEventDates)
             }
 
         }
 
+        val schemes=calendar?.schemes
+        if (schemes?.get(0)?.scheme=="record"){
+
+            canvas?.drawCircle(centerX.toFloat(),centerY.toFloat(),(mItemWidth/2).toFloat(),paintEventDates)
+        }
     }
 
     override fun onDrawText(canvas: Canvas?, calendar: Calendar?, x: Int, y: Int, hasScheme: Boolean, isSelected: Boolean) {
 
+        //Log.d("ondrawtext","${mItemWidth}, $mItemHeight")
+        //                          94,         56
         val baselineY=mTextBaseLine+y
-        val centerX=x+mItemWidth/2
+        val centerX=(x+mItemWidth/2).toFloat()
+        val centerY=(y+mItemHeight/2).toFloat()
 
-        if (hasScheme){
-            
-        }else{
+        val day=calendar?.day
 
+        if (calendar!!.isCurrentMonth){ // 是指定顯示該月的範圍
+
+            if(calendar.isCurrentDay){ // 是今天?
+
+                canvas?.drawCircle(centerX+(mItemHeight/4),centerY,(22).toFloat(),paintTodayCircle)
+                canvas?.drawText(
+                        day.toString(),
+                        centerX,
+                        baselineY,
+                        paintToday)
+            }else{ // 是本月的其他天
+
+                calendarCompare.set(calendar.year,calendar.month-1,calendar.day)
+
+                if (calendarCompare.before(calendarNow)){ //是本月的今天之前
+                    Log.d("calendarMonth","before, ${calendar.day}")
+                    canvas?.drawText(
+                            calendar.day.toString(),
+                            centerX,
+                            baselineY,
+                            currentMonthPassedDates)
+
+                }else if (calendarCompare.after(calendarNow)){ //是本月的今天之後
+                    Log.d("calendarMonth","after, ${calendar.day}")
+                    canvas?.drawText(
+                            calendar.day.toString(),
+                            centerX,
+                            baselineY,
+                            currentMonthComingDates)
+                }
+            }
+        }else{// 是指定顯示該月的 上個月 or 下個月的範圍
+
+            if (calendar.day<15){// 下個月的範圍
+                canvas?.drawText(
+                        calendar.day.toString(),
+                        centerX,
+                        baselineY,
+                        nextMonthDates)
+            }
         }
+
+
+        if (calendar.hasScheme()){
+            Log.d("ondrawtext","has scheme")
+        }else{
+            Log.d("ondrawtext","has No scheme")
+        }
+        val schemes=calendar.schemes
+
+        if (schemes!=null){
+            Log.d("bpcalendarff","scheme size ${schemes.size}, ${calendar.day}")
+            if (schemes[0]!=null){
+                if (schemes[0].scheme=="record"){
+
+                    canvas?.drawCircle(centerX,centerY,(mItemWidth/2).toFloat(),paintEventDates)
+                }
+            }
+        }else{
+            Log.d("bpcalendarff","${calendar.day}, ${calendar.scheme}")
+        }
+
+        /*
+        Log.d("bpcalendarff","scheme size ${schemes.size}, ${calendar.day}")
+        if (schemes[0]!=null){
+            if (schemes[0].scheme=="record"){
+
+                canvas?.drawCircle(centerX,centerY,(mItemWidth/2).toFloat(),paintEventDates)
+            }
+        }
+
+         */
+
+
+
     }
-
-
 }
